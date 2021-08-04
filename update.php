0b8d4acc80d346eea6f194e8e5d3b82012ce435d
youmakemyday <39655029+youmakemyday@users.noreply.github.com>
2021-08-04 21:56:15 +0200

    Update update.php

--- update.php
+++ update.php
@@ -13,15 +13,15 @@
  define("MODE","engine");
  // check authentication
  if(wdf_authenticated()!=2){
-  wdf_dump("You are not authenticated!",null,null,true);
+  wdf_dump("Login notwendig!",null,null,true);
   die();
  }
  // check for git
  if(is_dir(DIR.".git")){
-  wdf_dump("Git based update!",null,null,true);
+  wdf_dump("Update per git!",null,null,true);
   // check for localhost
   if(in_array($_SERVER['HTTP_HOST'],array("localhost","127.0.0.1"))){
-   wdf_dump("Git pull denied non localhost!",null,null,true);
+   wdf_dump("Update per git nicht moeglich!",null,null,true);
    die();
   }else{
    // make command
@@ -33,13 +33,13 @@
   }
  }else{
   // zip based update
-  wdf_dump("Zip based update!",null,null,true);
-  $update=file_get_contents("https://github.com/Zavy86/WikiDocs/archive/master.zip");
+  wdf_dump("Update per zip!",null,null,true);
+  $update=file_get_contents("https://github.com/youmakemyday/WikiDocs/archive/master.zip");
   $bytes=file_put_contents(DIR."update.zip",$update);
   if($bytes>0){
-   wdf_dump("Update download success!",null,null,true);
+   wdf_dump("Update Download erfolgreich!",null,null,true);
   }else{
-   wdf_dump("Update download failed!",null,null,true);
+   wdf_dump("Update Download fehlgeschlagen!",null,null,true);
   }
   // backup current version
   $backup_name=date("Ymd_His").".zip";
@@ -56,8 +56,8 @@
    $shell_output=exec('whoami')."@".exec('hostname').":".shell_exec($command);
    wdf_dump($shell_output,$command,null,true);
    // check if previous update was deleted
-   if(is_dir(DIR."update")){wdf_dump("Previous update deleted!",null,null,true);}
-   else{die("An error occured deleting previous update!");}
+   if(is_dir(DIR."update")){wdf_dump("Letzes Update entfernt!",null,null,true);}
+   else{die("Fehler beim Entfernen des letzten Updates aufgetreten");}
   }
   // unzip update
   $command="cd ".DIR." ; pwd ; unzip update.zip -d ./update";
@@ -65,7 +65,7 @@
   wdf_dump($shell_output,$command,null,true);
   // check for new version update
   if(file_exists(DIR."update/WikiDocs-master/VERSION.txt")){
-   wdf_dump("Update unzipped!",null,null,true);
+   wdf_dump("Update entpackt!",null,null,true);
   }else{
    die("An error occured unzipping the update!");
   }
@@ -75,41 +75,41 @@
   wdf_dump($shell_output,$command,null,true);
   // check for configuration
   if(file_exists(DIR."update/WikiDocs-master/config.inc.php")){wdf_dump("config.inc.php copied!",null,null,true);}
-  else{die("An error occured copying configuration!");}
+  else{die("Fehler beim Kopieren der Konfiguration aufgetreten!");}
   // copy .htaccess into update
   $command="cd ".DIR." ; pwd ; cp ./.htaccess ./update/WikiDocs-master/.htaccess";
   $shell_output=exec('whoami')."@".exec('hostname').":".shell_exec($command);
   wdf_dump($shell_output,$command,null,true);
   // check for htaccess
-  if(file_exists(DIR."update/WikiDocs-master/.htaccess")){wdf_dump(".htaccess copied!",null,null,true);}
-  else{die("An error occured copying .htaccess!");}
+  if(file_exists(DIR."update/WikiDocs-master/.htaccess")){wdf_dump(".htaccess kopiert!",null,null,true);}
+  else{die("Fehler beim Kopieren der .htaccess aufgetreten!");}
   // copy documents into update
   $command="cd ".DIR." ; pwd ; cp -R ./documents ./update/WikiDocs-master/";
   $shell_output=exec('whoami')."@".exec('hostname').":".shell_exec($command);
   wdf_dump($shell_output,$command,null,true);
   // check for htaccess
-  if(file_exists(DIR."update/WikiDocs-master/documents/homepage/content.md")){wdf_dump("documents copied!",null,null,true);}
-  else{die("An error occured copying documents!");}
+  if(file_exists(DIR."update/WikiDocs-master/documents/homepage/content.md")){wdf_dump("Inhalte erfolgreich kopiert!",null,null,true);}
+  else{die("Fehler beim Kopieren der Inhalte aufgetreten!");}
   // overwrite with update
   $command="cd ".DIR." ; pwd ; cp -R ./update/WikiDocs-master/* ./";
   $shell_output=exec('whoami')."@".exec('hostname').":".shell_exec($command);
   wdf_dump($shell_output,$command,null,true);
   // check for update
-  if(file_exists(DIR."VERSION.txt")){wdf_dump("Update overwrited!",null,null,true);}
-  else{die("An error occured during update!");}
+  if(file_exists(DIR."VERSION.txt")){wdf_dump("Update &uuml;berschrieben",null,null,true);}
+  else{die("Fehler beim Aktualisieren aufgetreten!");}
   // delete update directory
   $command="cd ".DIR." ; pwd ; rm -R ./update";
   $shell_output=exec('whoami')."@".exec('hostname').":".shell_exec($command);
   wdf_dump($shell_output,$command,null,true);
   // check if previous update was deleted
-  if(!is_dir(DIR."update")){wdf_dump("Update directory deleted!",null,null,true);}
-  else{die("An error occured deleting update directory!");}
+  if(!is_dir(DIR."update")){wdf_dump("Update Verzeichnis geleert!",null,null,true);}
+  else{die("Fehler beim Leeren des Update-Verzeichnisses aufgetreten!");}
   // delete update zip
   $command="cd ".DIR." ; pwd ; rm ./update.zip";
   $shell_output=exec('whoami')."@".exec('hostname').":".shell_exec($command);
   wdf_dump($shell_output,$command,null,true);
   // check if previous update was deleted
-  if(!file_exists(DIR."update.zip")){wdf_dump("Update zip deleted!",null,null,true);}
-  else{die("An error occured deleting update zip!");}
+  if(!file_exists(DIR."update.zip")){wdf_dump("Update zip entfernt!",null,null,true);}
+  else{die("Es ist ein Fehler beim Entfernen der Update zip aufgetreten");}
  }
 ?>
